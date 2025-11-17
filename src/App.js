5074d23d050bcccb6d0d808dcaa689de0429f63d
SHIVAM <shivamsavita2000@gmail.com>
2025-11-18 00:05:58 +0530

    chnages for jii

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with shivam 
+          Learn React with shivam sir jii
         </a>
       </header>
     </div>
